f34753dd7c82dc6990df2b8156441ff69f6e6442
The Lizard <suzpaylor@gmail.com>
2018-11-14 16:41:28 -0700

    Added testFunction to the Package_Installer File - next up npm install

--- src/package-installer.js
+++ src/package-installer.js
@@ -1,5 +1,18 @@
-export function packageInstaller(packages) {
-  return ""
+export function packageInstaller(input) {
+ 
+  let result = new Set(),
+  pairs = new Map(input.map(s => s.split(': '))),
+  loop = (_, key) => { 
+      if (pairs.get(key)) { 
+          loop(_, pairs.get(key)); 
+          pairs.set(key, null); 
+      }
+      
+      result.add(key); 
+  };
+pairs.forEach(loop); 
+result = Array.from(result).join(', ');
+return result; // Convert Set to Array
 }
 
 export default packageInstaller;
